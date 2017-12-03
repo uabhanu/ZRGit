045349b8d12053460a34009a8a33e92f6541aa3c
Bhanu <uabhanu@gmail.com>
2017-12-03 13:11:00 +0000

    Section 9 Lecture 249 Up Next

---
+++ ZombieRunnerFacebook/Assets/Scripts/BhanuSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BhanuSpawner : MonoBehaviour 
+{
+    Transform[] m_spawnPoints;
+
+    [SerializeField] bool m_reSpawn = false;
+    [SerializeField] GameObject m_bhanuPrefab;
+    [SerializeField] Transform m_bhanuSpawnPoints;
+
+	void Start() 
+    {
+        m_spawnPoints = m_bhanuSpawnPoints.GetComponentsInChildren<Transform>();
+	}
+
+    void Update()
+    {
+        if(Time.timeScale == 0)
+        {
+            return;
+        }
+
+        ReSpawn();
+    }
+
+	void ReSpawn() 
+    {
+        if(!m_reSpawn)
+        {
+            int randomValue = Random.Range(1 , m_spawnPoints.Length);   
+            GameObject bhanuPlayer = Instantiate(m_bhanuPrefab);
+            bhanuPlayer.transform.position = m_spawnPoints[randomValue].transform.position;      
+            m_reSpawn = true;
+        }
+	}
+}
