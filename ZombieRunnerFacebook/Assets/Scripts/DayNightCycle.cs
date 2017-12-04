71ac6573f1e4e7046819453b8ae4daf134cd40fc
Bhanu <uabhanu@gmail.com>
2017-12-04 09:43:24 +0000

    Section 9 Lecture 254 Up Next

---
+++ ZombieRunnerFacebook/Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour 
+{
+    Quaternion m_startRotation;
+
+    [Tooltip ("Number of minutes per second that pass, try 60")] [SerializeField] float m_minsPerSec;
+
+	void Start() 
+    {
+		m_startRotation = transform.rotation;
+	}
+
+	void Update() 
+    {
+		if(Time.timeScale == 0)
+            return;
+
+        float angleThisFrame = Time.deltaTime / 360 * m_minsPerSec;
+        transform.RotateAround(transform.position , Vector3.forward , angleThisFrame);
+	}
+}
