6e367088f6fba81d6a31a8695698cef3fa4fa511
Bhanu <uabhanu@gmail.com>
2017-12-05 10:04:04 +0000

    Section 9 Lecture 255 Up Next

---
+++ ZombieRunnerFacebook/Assets/Scripts/ClearArea.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearArea : MonoBehaviour 
+{
+    [SerializeField] float m_timeSinceLastTrigger = 0f;
+
+	void Start() 
+    {
+		
+	}
+
+	void Update() 
+    {
+		if(Time.timeScale == 0)
+            return;
+
+        m_timeSinceLastTrigger += Time.deltaTime;
+
+        if(m_timeSinceLastTrigger > 1.2f)
+        {
+            Debug.Log("Found Clear Area");
+        }
+	}
+
+    void OnTriggerStay()
+    {
+        m_timeSinceLastTrigger = 0f;
+    }
+}
